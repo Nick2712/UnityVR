ced0b9751319ce645643047d648052e26236f67f
Nick2712 <53843441+Nick2712@users.noreply.github.com>
2022-02-18 21:39:38 +0300

    executed all lesson scripts

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,18 +1,61 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace NikolayTrofimovUnityVR
+{
+    public class ObstacleSpawner : MonoBehaviour
     {
-        
+        [SerializeField] private Transform[] _obstacles;
+        [SerializeField] private float _spawnStep;
+        [SerializeField] private float _spawnDistance;
+        [SerializeField] private Transform _obstacleParent;
+
+        [SerializeField] private Vector2 _segmentWidth;
+
+        private Transform _myTrans;
+        private Vector3 _lastPos;
+
+        private List<Transform> _spawnedObstacles = new List<Transform>();
+        public List<Transform> SpawnedObstacles 
+        { 
+            get 
+            { 
+                _spawnedObstacles.RemoveAll(TransformIsNull); 
+                return _spawnedObstacles; 
+            } 
+        }
+
+        private void Start()
+        {
+            _myTrans = transform;
+            _lastPos = _myTrans.position;
+        }
+
+        private void Update()
+        {
+            if(_myTrans.position.z > _lastPos.z + _spawnStep)
+            {
+                _lastPos.z += _spawnStep;
+
+                Transform newObstacle = _obstacles[Random.Range(0, _obstacles.Length)];
+
+                _spawnedObstacles.Add
+                (
+                    Instantiate
+                    (
+                        newObstacle, 
+                        new Vector3(Random.Range(_segmentWidth.x, _segmentWidth.y), 0, _lastPos.z + _spawnDistance), 
+                        Quaternion.identity,
+                        _obstacleParent
+                    )
+                );
+            }
+        }
+
+        private bool TransformIsNull(Transform transform)
+        {
+            return transform == null;
+        }
     }
 }
